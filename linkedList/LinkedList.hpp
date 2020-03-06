d1d7222a5c29706cf8c4aa1297d4e5be01c75fd1
Sandy <thecsw@ku.edu>
2020-03-06 16:27:31 -0600

    Updated the bool LinkedList<T>::removeBack()
    Some changes were made. Removed isRemoved variable and started to
    return good old true/false.
    
    Signed-off-by: Sandy <thecsw@ku.edu>

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -94,15 +94,23 @@ void LinkedList<T>::addFront(T value)
 template <typename T>
 bool LinkedList<T>::removeBack()
 {
-	Node<T>* lastNode = nullptr;
+	if (isEmpty()) return false;
+	if (size() == 1) {
+		delete(m_front);
+		m_front=nullptr;
+		m_size--;
+		return true;
+	}
+	Node<T>* lastNode = m_front;
 	Node<T>* secondintoLast = nullptr;
-	bool isRemoved = false;
-
-	/** TODO 
-		Fix this method
-	*/
-
-	return(isRemoved);
+	while (lastNode->getNext() != nullptr) {
+		secondintoLast = lastNode;
+		lastNode = lastNode->getNext();
+	}
+	delete(lastNode);
+	secondintoLast->setNext(nullptr);
+	m_size--;
+	return true;
 }	
 
 template <typename T>
