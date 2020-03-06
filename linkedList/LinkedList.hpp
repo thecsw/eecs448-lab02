443869612ab8dd9e4a7cf38b0d10da98a4e432b8
Sandy <thecsw@ku.edu>
2020-03-06 16:11:10 -0600

    Updated the bool LinkedList<T>::search(T value) const
    Optimized the method by removed isFound variable and returning
    immediately if appropriate value is found.
    
    Signed-off-by: Sandy <thecsw@ku.edu>

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -36,13 +36,11 @@ template <typename T>
 bool LinkedList<T>::search(T value) const
 {
 	Node<T>* temp = m_front;
-	bool isFound = false;
-
-	/** TODO 
-		Fix this method
-	*/
-
-	return(isFound);
+	while (temp != nullptr) {
+		if (value == temp->getValue()) return true;
+		temp = temp->getNext();
+	}
+	return false;
 }
 
 template <typename T>
